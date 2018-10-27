98e596954a3a56b861302881497a38ce06da52aa
Łukasz Pająk <lukas.pajak27@gmail.com>
2018-10-27 20:31:53 +0200

    Modify encoding. Add retweeted column. Add csv to gitignore.

--- script.py
+++ script.py
@@ -14,20 +14,20 @@ api = tweepy.API(auth,wait_on_rate_limit=True)
 hashtags = ['h1b']
 
 for hashtag in hashtags:
-    csvFile = open(hashtag+'.csv', 'a')
+    csvFile = open(hashtag+'.csv', 'a', encoding='utf-8') 
     csvWriter = csv.writer(csvFile)
 
-    searched_tweets = [status for status in tweepy.Cursor(api.search, q='#'+hashtag).items(1)]
+    searched_tweets = [status for status in tweepy.Cursor(api.search, q='#'+hashtag, since='2018-10-20', until='2018-10-21').items()]
 
-    csvWriter.writerow(['tweet_id','tweet_text','tweet_created_at','tweet_retweet_count','tweet_favorite_count',
+    csvWriter.writerow(['tweet_id','retweeted','tweet_text','tweet_created_at','tweet_retweet_count','tweet_favorite_count',
         'user_id','user_name','user_location','user_friends_count',
         'place_type','place_full_name','country'])
 
 for tweet in searched_tweets:
     if tweet.place is not None:
-        csvWriter.writerow([tweet.id, tweet.text, tweet.created_at, tweet.retweet_count, tweet.favorite_count,
+        csvWriter.writerow([tweet.id, tweet.retweeted, tweet.text.encode("utf-8"), tweet.created_at, tweet.retweet_count, tweet.favorite_count,
         tweet.user.id, tweet.user.name, tweet.user.location, tweet.user.friends_count,
         tweet.place.place_type, tweet.place.full_name, tweet.place.country])
     else:
-        csvWriter.writerow([tweet.id, tweet.text, tweet.created_at, tweet.retweet_count, tweet.favorite_count,
+        csvWriter.writerow([tweet.id, tweet.retweeted, tweet.text.encode("utf-8"), tweet.created_at, tweet.retweet_count, tweet.favorite_count,
         tweet.user.id, tweet.user.name, tweet.user.location, tweet.user.friends_count])
